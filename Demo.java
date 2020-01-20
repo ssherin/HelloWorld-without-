0436c9967ff16d4871231181ffb9db860f772700
ssherin <ssherin044@gmail.com>
2020-01-20 08:32:09 +0530

    added code 1

---
+++ Demo.java
@@ -0,0 +1,10 @@
+// Hello Worls without ;
+
+class Demo{
+	public static void main(String[] args) {
+		if(System.out.printf("Hello World")== null){		// we use "printf" as its return type is "PRINT STREAM" and not primitives
+															//hence it helps print whatever inside (..) if not Null
+															// thus no need of ;
+		}
+	}
+}
